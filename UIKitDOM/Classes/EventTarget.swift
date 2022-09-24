3c6cfcbe9586f0524622efb4fc75fa5739af2788
shirakaba <14055146+shirakaba@users.noreply.github.com>
2022-09-24 21:45:24 +0900

    (add|remove)EventListener hammered into Swift

--- UIKitDOM/Classes/EventTarget.swift
+++ UIKitDOM/Classes/EventTarget.swift
@@ -1,25 +1,29 @@
 @objc protocol EventTarget {
-  // Unlike DOM, we don't accept a boolean for the options param; the consumer
-  // will have to convert any options to AddEventListenerOptions type.
-  func addEventListener(_ type: NSString, _ callback: ((_ event: UIEvent) -> Bool)?, _ options: AddEventListenerOptions?)
+  /**
+      Adds a callback for the given Event type and EventListener options.
+      - Attention: Does not check equality with previous callbacks added (not possible in Swift/Obj-C).
+      - Returns: The id of the added callback (or "0" if no callback was added).
+  */
+  func addEventListener(_ type: NSString, _ callback: ((_ event: UIEvent) -> Void)?, _ options: AddEventListenerOptions?) -> NSString
   
   func dispatchEvent(_ event: UIEvent)
   
-  // Unlike DOM, we don't accept a boolean for the options param; the consumer
-  // will have to convert any options to AddEventListenerOptions type.
-  func removeEventListener(_ type: NSString, _ callback: ((_ event: UIEvent) -> Bool)?, _ options: AddEventListenerOptions?)
+  /**
+      Removes the callback for the given id.
+  */
+  func removeEventListenerById(_ type: NSString, _ id: NSString)
 }
 
 @objc protocol EventListenerOptions {
-  var capture: OptionalBool? { get set }
+  @objc optional var capture: Bool { get set }
 }
 
 @objc protocol AddEventListenerOptions: EventListenerOptions {
-  var once: OptionalBool? { get set }
+  @objc optional var once: Bool { get set }
   
-  var passive: OptionalBool? { get set }
+  @objc optional var passive: Bool { get set }
   
   // This is mainly here as a stub; I don't intend to support anything related
   // to AbortController anytime soon.
-  var signal: AbortSignal? { get set }
+  @objc optional var signal: AbortSignal { get set }
 }
--- UIKitDOM/Classes/UIView+EventTarget.swift
+++ UIKitDOM/Classes/UIView+EventTarget.swift
@@ -1,1 +1,54 @@
+@objc extension UIView: EventTarget {
+  private static var eventListenerCount: Int = 0;
+  
+  @nonobjc private static let association = ObjectAssociation<NSMutableDictionary>()
+  var listenerMap: NSMutableDictionary? {
+    get { return UIView.association[self] }
+    set { UIView.association[self] = newValue }
+  }
+  
+  func addEventListener(_ type: NSString, _ callback: ((UIEvent) -> Void)?, _ options: AddEventListenerOptions?) -> NSString {
+    guard let callback = callback, let listenerMap = self.listenerMap else { return "0" }
+    
+    let options = normalizeEventHandlerOptions(options)
+    guard !(options.signal?.aborted ?? false) else { return "0" }
+    
+    // Ideally we'd compare equality with existing callbacks, but that's not
+    // possible in Swift.
+    let listenersForType: NSMutableDictionary = listenerMap.object(forKey: type) as? NSMutableDictionary ?? NSMutableDictionary()
+    
+    UIView.eventListenerCount += 1;
+    let listenerId = String(UIView.eventListenerCount) as NSString
+    listenersForType.setObject([callback, options], forKey: listenerId)
+    listenerMap.setObject(listenersForType, forKey: type)
+    
+    return listenerId
+  }
+  
+  func dispatchEvent(_ event: UIEvent) {
+    // TODO
+  }
+  
+  func removeEventListenerById(_ type: NSString, _ id: NSString) {
+    guard id != "0", let listenerMap = self.listenerMap, let listenersForType: NSMutableDictionary = listenerMap.object(forKey: type) as? NSMutableDictionary else { return }
+    listenersForType.removeObject(forKey: id)
+    
+    if(listenersForType.count == 0){
+      listenerMap.removeObject(forKey: type)
+    }
+  }
+}
 
+func normalizeEventHandlerOptions(_ options: AddEventListenerOptions?) -> AddEventListenerOptions {
+  var returnValue: [String: Any] = [
+    "capture": options?.capture ?? false,
+    "once": options?.once ?? false,
+    "passive": options?.passive ?? false,
+  ]
+  
+  if let signal = options?.signal {
+    returnValue["signal"] = signal
+  }
+
+  return returnValue as! AddEventListenerOptions;
+}
