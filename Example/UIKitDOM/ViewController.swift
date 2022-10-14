33228c4c4cd018f923b6ffc0dea2ec5342dbcdc7
shirakaba <14055146+shirakaba@users.noreply.github.com>
2022-10-14 22:04:07 +0900

    Set up obstacle course

--- Example/UIKitDOM/ViewController.swift
+++ Example/UIKitDOM/ViewController.swift
@@ -2,14 +2,34 @@ import UIKit
 import UIKitDOM
 
 class ViewController: UIViewController {
+  let button: UIButton = UIButton(frame: CGRect(x: 50, y: 100, width: 100, height: 40))
+  let button2: UIButton = UIButton(frame: CGRect(x: 50, y: 400, width: 100, height: 40))
+  let label: UILabel = UILabel(frame: CGRect(x: 50, y: 200, width: 100, height: 40))
+  let textField: UITextField = UITextField(frame: CGRect(x: 50, y: 300, width: 100, height: 40))
+  var scrollView: UIScrollView = UIScrollView()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    let scrollView = UIScrollView(frame: self.view.frame)
+    self.view.backgroundColor = UIColor.gray
+    scrollView.backgroundColor = UIColor.yellow
+    button.backgroundColor = UIColor.orange
+    button2.backgroundColor = UIColor.red
+    textField.backgroundColor = UIColor.cyan
+    label.backgroundColor = UIColor.blue
+    button.setTitle("The top UIButton", for: .normal)
+    button2.setTitle("The bottom UIButton", for: .normal)
+    label.text = "The UILabel"
+    textField.text = "The UITextField"
+    
+    scrollView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height / 2)
+    scrollView.contentSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+    
     self.view.addSubview(scrollView)
-    self.view.addSubview(UIButton())
-    self.view.addSubview(UILabel())
-    self.view.addSubview(UITextField())
+    scrollView.addSubview(button)
+    scrollView.addSubview(label)
+    scrollView.addSubview(textField)
+    scrollView.addSubview(button2)
     
     let _ = self.view.addEventListener(
       "tap",
